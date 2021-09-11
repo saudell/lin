d46dcfc2a54a4d17b35f490871a5dc2628bb19e9
saudell <linlin010600@163.com>
2021-09-11 17:41:18 +0800

    second commit

--- test.c
+++ test.c
@@ -4,7 +4,13 @@ int main()
 {
 	printf("test message..\n");
 	printf("test message..\n");
-	printf("test message..\n");
+	
+
+
+	 printf("test message..\n");
+	 printf("test message..\n");
+	 printf("test message..\n");
+
 	while(1)
 		sleep(1);
 	return 0;
